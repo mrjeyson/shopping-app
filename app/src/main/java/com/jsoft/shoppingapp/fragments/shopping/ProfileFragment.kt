9cffa451cac4d01e65e9d44d09b763e1617d7d89
Jasurbek98 <jasurbekkurganbayev@gmail.com>
2024-04-09 16:12:18 +0500

    asddd

---
+++ app/src/main/java/com/jsoft/shoppingapp/fragments/shopping/ProfileFragment.kt
@@ -0,0 +1,9 @@
+package com.jsoft.shoppingapp.fragments.shopping
+
+import androidx.fragment.app.Fragment
+import com.jsoft.shoppingapp.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ProfileFragment : Fragment(R.layout.fragment_profile) {
+}
