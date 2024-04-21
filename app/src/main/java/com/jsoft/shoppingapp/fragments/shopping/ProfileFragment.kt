3a7c583036da6ac04a1db4a6ff4685458d7fcf85
Jasurbek98 <jasurbekkurganbayev@gmail.com>
2024-04-21 15:31:05 +0500

    full completion

--- app/src/main/java/com/jsoft/shoppingapp/fragments/shopping/ProfileFragment.kt
+++ app/src/main/java/com/jsoft/shoppingapp/fragments/shopping/ProfileFragment.kt
@@ -1,9 +1,102 @@
 package com.jsoft.shoppingapp.fragments.shopping
 
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import br.com.simplepass.loadingbutton.BuildConfig
+import com.bumptech.glide.Glide
 import com.jsoft.shoppingapp.R
+import com.jsoft.shoppingapp.activities.LoginRegisterActivity
+import com.jsoft.shoppingapp.databinding.FragmentProfileBinding
+import com.jsoft.shoppingapp.utils.Resource
+import com.jsoft.shoppingapp.utils.showBottomNavigationView
+import com.jsoft.shoppingapp.viewmodel.ProfileViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
 
 @AndroidEntryPoint
-class ProfileFragment : Fragment(R.layout.fragment_profile) {
+class ProfileFragment : Fragment() {
+
+    private lateinit var binding: FragmentProfileBinding
+    val viewModel by viewModels<ProfileViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentProfileBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            constraintProfile.setOnClickListener {
+                findNavController().navigate(R.id.action_profileFragment_to_userAccountFragment)
+            }
+
+            linearAllOrders.setOnClickListener {
+                findNavController().navigate(R.id.action_profileFragment_to_ordersFragment)
+            }
+
+            linearBilling.setOnClickListener {
+                val action = ProfileFragmentDirections.actionProfileFragmentToBillingFragment(
+                    0f,
+                    emptyArray()
+                )
+                findNavController().navigate(action)
+            }
+            linearLogOut.setOnClickListener {
+                viewModel.logout()
+                val intent = Intent(requireActivity(), LoginRegisterActivity::class.java)
+                startActivity(intent)
+                requireActivity().finish()
+            }
+
+            tvVersion.text = "Version ${BuildConfig.VERSION_CODE}"
+
+            lifecycleScope.launchWhenStarted {
+                viewModel.user.collectLatest {
+                    when (it) {
+                        is Resource.Loading -> {
+                            binding.progressbarSettings.visibility = View.VISIBLE
+                        }
+
+                        is Resource.Success -> {
+                            binding.progressbarSettings.visibility = View.GONE
+                            Glide.with(requireView()).load(it.data!!.imagePath).error(
+                                ColorDrawable(
+                                    Color.BLACK
+                                )
+                            ).into(binding.imageUser)
+                            binding.tvUserName.text = "${it.data.firstName} ${it.data.lastName}"
+                        }
+
+                        is Resource.Error -> {
+                            binding.progressbarSettings.visibility = View.GONE
+                            Toast.makeText(requireContext(), it.message, Toast.LENGTH_SHORT).show()
+                        }
+
+                        else -> Unit
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        showBottomNavigationView()
+    }
+
 }
