b1ccdc5c9a5fbbef7570a26e86dc201d50a2f4f0
mrjeyson <71706561+mrjeyson@users.noreply.github.com>
2024-06-29 11:58:01 +0500

    ...

--- app/src/main/java/com/jsoft/shoppingapp/viewmodel/DetailsViewModel.kt
+++ app/src/main/java/com/jsoft/shoppingapp/viewmodel/DetailsViewModel.kt
@@ -20,7 +20,6 @@ class DetailsViewModel @Inject constructor(
     private val firebaseCommon: FirebaseCommon
 ) : ViewModel() {
 
-
     private val _addToCart = MutableStateFlow<Resource<CartProduct>>(Resource.Unspecified())
     val addToCart = _addToCart.asStateFlow()
 
