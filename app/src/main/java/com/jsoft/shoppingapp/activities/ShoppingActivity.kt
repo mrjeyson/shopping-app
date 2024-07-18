97f042d2f35da77080a67b88c56a50958d10001a
mrjeyson <71706561+mrjeyson@users.noreply.github.com>
2024-07-18 10:16:51 +0500

    ...

--- app/src/main/java/com/jsoft/shoppingapp/activities/ShoppingActivity.kt
+++ app/src/main/java/com/jsoft/shoppingapp/activities/ShoppingActivity.kt
@@ -20,6 +20,7 @@ import kotlinx.coroutines.flow.collectLatest
 @AndroidEntryPoint
 class ShoppingActivity : AppCompatActivity() {
 
+
     val binding by lazy { ActivityShoppingBinding.inflate(layoutInflater) }
 
     val viewModel by viewModels<CartViewModel>()
