d0f586b854e4aabb11f63cd6d12f1c92003acd17
mrjeyson <71706561+mrjeyson@users.noreply.github.com>
2024-11-06 15:37:21 +0500

    ...

--- app/src/main/java/com/jsoft/shoppingapp/viewmodel/BillingViewModel.kt
+++ app/src/main/java/com/jsoft/shoppingapp/viewmodel/BillingViewModel.kt
@@ -17,7 +17,6 @@ class BillingViewModel @Inject constructor(
     private val firestore: FirebaseFirestore, private val auth: FirebaseAuth
 ) : ViewModel() {
 
-
     private val _address = MutableStateFlow<Resource<List<Address>>>(Resource.Unspecified())
     val address = _address.asStateFlow()
 
