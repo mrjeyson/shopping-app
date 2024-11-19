3d0a741b9adc38919a321a68abc88fe2f9700c43
mrjeyson <71706561+mrjeyson@users.noreply.github.com>
2024-11-19 11:54:16 +0500

    ...

--- app/src/main/java/com/jsoft/shoppingapp/viewmodel/BillingViewModel.kt
+++ app/src/main/java/com/jsoft/shoppingapp/viewmodel/BillingViewModel.kt
@@ -25,6 +25,7 @@ class BillingViewModel @Inject constructor(
 
 
 
+
     private val _address = MutableStateFlow<Resource<List<Address>>>(Resource.Unspecified())
     val address = _address.asStateFlow()
 
