2a6e45e474bc1f7a9eea10bdcd5559512afc10ab
mrjeyson <71706561+mrjeyson@users.noreply.github.com>
2024-11-22 14:04:39 +0500

    ....

--- app/src/main/java/com/jsoft/shoppingapp/viewmodel/CartViewModel.kt
+++ app/src/main/java/com/jsoft/shoppingapp/viewmodel/CartViewModel.kt
@@ -29,6 +29,7 @@ class CartViewModel @Inject constructor(
 
 
 
+
     private val _cartProducts =
         MutableStateFlow<Resource<List<CartProduct>>>(Resource.Unspecified())
     val cartProducts = _cartProducts.asStateFlow()
